32d3bdfa3148ef66efc6432eb14a400cf2522ddd
Adam Rosas <adamrosas27@gmail.com>
2023-10-04 21:31:22 -0700

    Add general outline for Task_Manager class

---
+++ src/task_manager.py
@@ -0,0 +1,19 @@
+# Add Task_Manager class here
+
+
+# Add constructor method here
+
+
+# Add add_task method here
+
+
+# Add remove_task method here
+
+
+# Add edit_task method here
+
+
+# Add view_tasks method here
+
+
+# Add mark_completion_status method here
